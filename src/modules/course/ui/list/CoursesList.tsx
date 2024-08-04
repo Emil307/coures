b7a7ad4e31335e2a0d0de7d424d350080a1a84fc
Emil307 <emilnovikov392@gmail.com>
2024-08-04 00:52:25 -0700

    [Fix] courses grid

--- src/modules/course/ui/list/CoursesList.tsx
+++ src/modules/course/ui/list/CoursesList.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import toast from 'react-hot-toast';
-import { CourseCard, useCourseStore, getCourses } from '@/modules/course';
-import { ICourse } from '@/shared';
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+import { CourseCard, useCourseStore, getCourses } from "@/modules/course";
+import { ICourse } from "@/shared";
 
 export const CoursesList: React.FC = () => {
   const courses = useCourseStore((state) => state.courses);
@@ -20,8 +20,8 @@ export const CoursesList: React.FC = () => {
       })
       .catch((e) => {
         toast.error(
-          e.response.data.detail.msg
-            || 'Упс... кажется, что-то сломалось :( Мы уже работаем над этим',
+          e.response.data.detail.msg ||
+            "Упс... кажется, что-то сломалось :( Мы уже работаем над этим"
         );
       })
       .finally(() => {
@@ -30,13 +30,10 @@ export const CoursesList: React.FC = () => {
   }
 
   return (
-    <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 gap-5 w-fit mx-auto">
-      {isLoading && (
-        <>
-        </>
-      )}
-      {courses
-        && courses.map((course: ICourse) => (
+    <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 w-fit mx-auto">
+      {isLoading && <></>}
+      {courses &&
+        courses.map((course: ICourse) => (
           <CourseCard course={course} key={course.id} />
         ))}
     </div>
