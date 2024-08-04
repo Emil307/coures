5e1e510fa039a14418721417534d7a003212f192
Emil307 <emilnovikov392@gmail.com>
2024-08-04 15:59:17 -0700

    [Chore] change header size

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -22,8 +22,8 @@ export const Header: React.FC = () => {
   }
 
   return (
-    <div className="w-full h-16 flex justify-between items-center p-4">
-      <h1 className="text-2xl font-bold">BrainUp</h1>
+    <div className="w-full h-24 flex justify-between items-center p-4">
+      <h1 className="text-3xl font-bold">BrainUp</h1>
       <button onClick={handleLogout}>logout</button>
     </div>
   );
