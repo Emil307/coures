a7799b356e18e0a5b106a924132dc532668b5a6f
Emil307 <emilnovikov392@gmail.com>
2024-08-04 21:06:45 -0700

    [Fix] lesson container

--- src/pages/Lesson/Lesson.tsx
+++ src/pages/Lesson/Lesson.tsx
@@ -35,27 +35,29 @@ export const Lesson: React.FC = () => {
   return (
     <>
       <LessonHeader />
-      {isLoading && <>Loading...</>}
-      {lesson && (
-        <div className="w-fit max-w-4xl flex flex-col gap-16 pt-4 justify-center m-auto pb-12">
-          <div className="flex flex-col gap-4">
+      <div className="w-fit max-w-4xl flex flex-col gap-16 pt-4 justify-center m-auto pb-12">
+        {isLoading && <>Loading...</>}
+        {lesson && (
+          <>
             <div className="flex flex-col gap-4">
-              <h2 className="text-4xl font-bold text-left">{lesson.title}</h2>
-              <p className="text-xl leading-8">{lesson.text}</p>
+              <div className="flex flex-col gap-4">
+                <h2 className="text-4xl font-bold text-left">{lesson.title}</h2>
+                <p className="text-xl leading-8">{lesson.text}</p>
+              </div>
+              <iframe
+                width="784"
+                height="480"
+                src="https://player.vimeo.com/video/994701694?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+                title="aso"
+                allowFullScreen
+              ></iframe>
             </div>
-            <iframe
-              width="784"
-              height="480"
-              src="https://player.vimeo.com/video/994701694?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-              frameBorder="0"
-              allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-              title="aso"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <Test />
-        </div>
-      )}
+            <Test />
+          </>
+        )}
+      </div>
     </>
   );
 };
