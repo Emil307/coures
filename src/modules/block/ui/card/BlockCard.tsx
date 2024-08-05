a21b345d459c231ba00bc11a6ab95b6c3de4bdba
Emil307 <emilnovikov392@gmail.com>
2024-08-04 20:22:56 -0700

    [Chore] change blocks size

--- src/modules/block/ui/card/BlockCard.tsx
+++ src/modules/block/ui/card/BlockCard.tsx
@@ -12,7 +12,7 @@ export const BlockCard: React.FC<IBlockCardProps> = ({ block }) => {
   return (
     <>
       <button onClick={() => setIsOpen(!isOpen)}>
-        <div className="flex items-center h-14 pl-6 pr-6 rounded-md bg-white shadow-md">
+        <div className="flex items-center h-16 pl-6 pr-6 rounded-md bg-white shadow-md">
           <h5 className="text-xl">{block.title}</h5>
         </div>
       </button>
